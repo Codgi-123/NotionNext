46157e17d51654373963744d0a91a2f56b3242f6
Codgi <54504858+Codgi-123@users.noreply.github.com>
2025-08-31 20:02:45 +0800

    Update style.js

--- themes/gitbook/style.js
+++ themes/gitbook/style.js
@@ -9,7 +9,7 @@ const Style = () => {
     <style jsx global>{`
       // 底色
       .dark body {
-        background-color: black;
+        background-color: #191919;
       }
 
       .bottom-button-group {
